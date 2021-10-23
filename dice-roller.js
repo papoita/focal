3305cfe62b78d2a3e107fa5f05e8fd690ad7c219
PaolaP <paolaperez8@gmail.com>
2021-10-23 16:12:52 -0400

    testing out code

---
+++ dice-roller.js
@@ -0,0 +1,36 @@
+
+
+//pseudocode
+//input single number
+//mske that number a list of numbers until reaching the number
+
+//element is rolled element[i] number of times
+
+function rollingDice(number) {
+  let newString = "";
+  let numberOfNumbers = number *
+  for (let i = 0; i < number.length; i++) {
+
+  }
+  array.forEach(number => {
+    let randomNumber = (Math.random * number);
+    newString += "number";
+  }); console.log(newString);
+
+}
+
+function joinList(array) {
+  let sentence = "";
+  {
+    if (i < array.length - 1) {
+      sentence += array[i];
+      sentence += ", ";
+    } else if (i === array.length - 1) {
+      sentence += array[i];
+    } else if (array.length === 1) {
+      sentence += array[0];
+    }
+  } return sentence;
+}
+
+rollingDice(3);
